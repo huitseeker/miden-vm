5450a3606d3977786fc88e71799afcaeda1ed813
Paul Schoenfelder <paulschoenfelder@fastmail.com>
2026-07-24 12:28:14 -0400

    assembly: clean up unused SourcePathTrimmer helpers

--- crates/assembly/src/assembler/debuginfo.rs
+++ crates/assembly/src/assembler/debuginfo.rs
@@ -1,6 +1,6 @@
 use std::path::{Path, PathBuf};
 
-use miden_assembly_syntax::debuginfo::{FileLineCol, Location, Uri};
+use miden_assembly_syntax::debuginfo::Uri;
 use miden_mast_package::debug_info::PackageDebugInfo;
 
 pub(super) fn trim_paths(debug_info: &mut PackageDebugInfo, trimmer: &SourcePathTrimmer) {
@@ -20,18 +20,6 @@ impl SourcePathTrimmer {
     pub fn new(cwd: PathBuf) -> Self {
         let cwd = cwd.canonicalize().unwrap_or(cwd);
         Self { cwd }
-    }
-
-    #[allow(unused)]
-    pub fn trim_location(&self, mut location: Location) -> Location {
-        location.uri = self.trim_uri(&location.uri);
-        location
-    }
-
-    #[allow(unused)]
-    pub fn trim_file_line_col(&self, mut location: FileLineCol) -> FileLineCol {
-        location.uri = self.trim_uri(&location.uri);
-        location
     }
 
     fn trim_uri(&self, uri: &Uri) -> Uri {
